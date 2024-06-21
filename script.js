574998f2d41f6d4fb1708db4edfd8c8d859ca907
sourab@06 <shettysourabh@gmail.com>
2024-06-21 16:29:26 -0400

    twitter button functionality and creating a loader animation

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+let insidebox=document.getElementsByClassName('insidebox')[0];
 let quoteline=document.getElementsByClassName('quote-line')[0];
 let author=document.getElementsByClassName('author')[0];
 let newquote=document.getElementsByClassName('btn2')[0];
+let loader=document.getElementsByClassName('loader')[0];
 let apiquotes=[];
+function loading(){
+    loader.hidden=false;
+    insidebox.hidden=true;
+  }
+  function complete(){
+    loader.hidden=true;
+    insidebox.hidden=false;
+
+  }
 async function quote(){
+    loading();
  const apiurl=  'https://jacintodesign.github.io/quotes-api/data/quotes.json';
  try{
  const response= await fetch(apiurl);
@@ -18,11 +30,14 @@ quoteline.innerHTML=apiquotes.text;
 
 // console.log("hello");
 
-
+complete();
 }
 
-   
+
 quote();
 function twitter() {
-    location.replace("https://x.com/i/flow/login")
+    // location.replace("https://x.com/i/flow/login")
+    const twitterurl=`https://twitter.com/intent/tweet?text=${quoteline.innerHTML} - ${author.innerHTML}`;
+    window.open(twitterurl,'_blank');
   }
+  
